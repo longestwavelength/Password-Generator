ebc77b0d6868ab62738fac75033c3b3cfc842c3e
Nikhil <longestwavelength@protonmail.com>
2024-02-12 20:05:31 +0530

    changed some things

--- script.js
+++ script.js
@@ -62,10 +62,8 @@ function generatePassword(upper, lower, number, symbol, length) {
         for(let i = arr.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * i);
             [arr[i], arr[j]] = [arr[j], arr[i]];
-            return arr.join('')
         }
-
-        return arr
+            return arr.join('')
 }   
 
 function getRandomUpper() {
